ce45ef6e692158764451597262e7a7e6d3a8d36e
Rick welte <rktvpr@gmail.com>
2023-02-17 13:44:33 -0500

    added grid layout for SingleListing.js

--- client/src/pages/SingleListing.js
+++ client/src/pages/SingleListing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header, Footer } from '../components'
+import { Col, Row, Space } from 'antd';
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
@@ -23,7 +24,32 @@ const SingleListing = () => {
         <div className="Listing container">
             <Header />
             <div className="SingleListing">
-
+                <Row>
+                    <Col span={12} push={12}>
+                        <h2> ADDRESS </h2>
+                    </Col>
+                    <Col span={12} pull={12}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12} push={12}>
+                        INSERT CAROSEL
+                    </Col>
+                    <Col span={12} pull={12}>
+                        DESCRIPTION
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12} push={12}>
+                        <Space direction="vertical" style={{ width: '100%' }}>
+                            <Button type="primary" block>
+                                Contact a Realtor today!
+                            </Button>
+                        </Space>
+                    </Col>
+                    <Col span={12} pull={12}>
+                    </Col>
+                </Row>
             </div>
             <Footer />
         </div>
