a69d806250e8e6d41f077545b7cc1fa530d669c3
Rick welte <rktvpr@gmail.com>
2023-02-25 14:11:51 -0500

    error fixing and base code implementation

---
+++ client/src/pages/ContactRealtor.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import { QUERY } from '../utils/queries';
+
+
+const ContactRealtor = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ContactRealtor
