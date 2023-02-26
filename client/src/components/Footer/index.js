8942703375178643d4f8b55d51141f7ceafddd09
Rahsan Lewis <rahsanlewis@gmail.com>
2023-02-25 20:34:57 -0500

    created profile page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,24 @@
-//import React from 'react';
+import React from 'react';
 
-//const Footer = () => {
-//    return (
- //       <header className="">
- //           test
-  //      </header>
-  //  )
-//}
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; 2023 ARSS Real Estate</p>
+          </div>
+          <div className="col-md-6">
+            <nav className="nav justify-content-end">
+              <a className="nav-link" href="#">Terms of Use</a>
+              <a className="nav-link" href="#">Privacy Policy</a>
+              <a className="nav-link" href="#">Contact Us</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-//export default Footer
+export default Footer;
