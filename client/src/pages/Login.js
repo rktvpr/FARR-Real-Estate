ae5cd7b33f3caadb7e138565502e5f1ab2655e64
Fatemeh Niaziorimi <fatemehniaziorimi@macbook-pro.mynetworksettings.com>
2023-02-28 01:03:11 -0500

    creating style sheet for Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
-import { Form, Input, Button } from "antd";
-
+import { Form, Input, Button, Divider, message } from "antd";
+import "../pages/Login.css";
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
@@ -9,6 +9,7 @@ import { LOGIN_USER } from '../utils/mutations';
 
 
 import Auth from '../utils/auth';
+import { FacebookOutlined, GoogleOutlined, TwitterOutlined } from "@ant-design/icons";
 
 const Login = () => {
     const [formState, setFormState] = useState({
@@ -47,36 +48,52 @@ const Login = () => {
           //  }
    
   };
+  
+   // function App(){
+        
+      //  const login=()=>{
+      //      message.success('Login Successful!');
+     //   }
+    //}
   return (
-    <div>
-      <Form
+    <div className="loginBg">
+        <h1 style={{textColor:"orange"}}>Welcome Back</h1>
+
+      <Form  className="loginForm"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
+        style={{ maxWidth: 600}}
         autoComplete="off"
       >
-        <Form.Item
-          label="Username"
+        <Form.Item className="username"
+        
+          label="Username" 
           name="username"
           rules={[{ required: true, message: "Please input your username!" }]}
+        
         >
-          <Input
-            placeholder="Username"
+           
+          <Input  
+            placeholder="Username" 
             onChange={(e) => {
                 setFormState({
                     ...formState,
                     username:e.target.value
                 });
                 }}
-        
+            
           />
+         
         </Form.Item>
 
-    
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
+                <Divider style={{borderColor:"red"}}/>
+        <Form.Item 
+        
+          label="Password" 
+          name="password"  
+          rules={[{ required: true,  message: "Please input your password!" }]}
+          style={{textColor:"blue"}}
+          
         >
           <Input
             type = "password"
@@ -91,11 +108,19 @@ const Login = () => {
         </Form.Item>
 
         <Form.Item wrapperCol={{offset:8, span:16}}>
-        <Button type="primary" htmlType="submit" onClick={submitLogin}>
+        <Button type="primary" class="next-btn" style={{backgroundColor:"orange", color:"blue"}} htmlType="submit" onClick={submitLogin}>
             Log in
-        </Button>    
+        </Button>  
+        <Divider style={{ borderColor: "black"}}>or Login with</Divider> 
+        <div className="socialLogin">
+        <GoogleOutlined className="socialIcon" onClick={login} />
+        
+    <TwitterOutlined className="socialIcon" onClick={login} />
+    <FacebookOutlined className="socialIcon" onClick={login} />
+    </div>
         </Form.Item>
       </Form>
+    
     </div>
   
   );
