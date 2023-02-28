4e771e3673f4b304f47775c41d6fbd6d759099a1
Anne <aepanagotopulos@gmail.com>
2023-02-28 13:09:08 -0500

    changes in heroSections

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import { Route } from 'react-router-dom';
+import '../utils/App.css';
+import heroSection from './heroSection';
+
+function Home () {
+    reutrn (
+        <>
+        <Router>
+            <navbar />
+            <switch>
+                <route path='/' exact component={Home} />
+            </switch>
+        </Router>
+        </>
+    )
+}
