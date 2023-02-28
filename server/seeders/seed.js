03e30cdce805ef276061d672bf99e8689cdf9dc6
Virgil <Virgilius.zip@gmail.com>
2023-02-27 19:02:49 -0500

    First modification to seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,26 +1,16 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Realtors, EstateLocation } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const EstateSeeds = require('./EstateSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Thought.deleteMany({});
+    await Realtors.deleteMany({});
+    await EstateLocation.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
   } catch (err) {
     console.error(err);
     process.exit(1);
