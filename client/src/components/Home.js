64316794a2990e2a335d0aa1153c2de4941672c2
Anne <aepanagotopulos@gmail.com>
2023-02-28 13:11:28 -0500

    commented out code on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,16 @@
-import { Route } from 'react-router-dom';
-import '../utils/App.css';
-import heroSection from './heroSection';
+// import { Route } from 'react-router-dom';
+// import '../utils/App.css';
+// import heroSection from './heroSection';
 
-function Home () {
-    reutrn (
-        <>
-        <Router>
-            <navbar />
-            <switch>
-                <route path='/' exact component={Home} />
-            </switch>
-        </Router>
-        </>
-    )
-}
+// function Home () {
+//     reutrn (
+//         <>
+//         <Router>
+//             <navbar />
+//             <switch>
+//                 <route path='/' exact component={Home} />
+//             </switch>
+//         </Router>
+//         </>
+//     )
+// }
